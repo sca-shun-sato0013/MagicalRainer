5fbe66382426894563a70dc667d714fed3598e6b
goto <>
2023-10-19 04:41:56 +0900

    animatorでの選択したときの画像移動

--- Assets/Goto/Scrips/BackCharaMain.cs
+++ Assets/Goto/Scrips/BackCharaMain.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackCharaMain : MonoBehaviour
 { 
@@ -19,6 +20,7 @@ public class BackCharaMain : MonoBehaviour
         var isActivee = Panel2.activeInHierarchy; // Panelがアクティブか取得
         Panel2.SetActive(true);
 
+        SceneManager.LoadScene("character selection");
 
     }
 }
