cd19277a1b4d90413c80ef708dbb6091f624045b
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-10-24 12:35:07 +0900

    ひっかき生成位置が変更しやすいように修正（途中）＋ステ2ボス/フェーズ1　水鉄砲作成

---
+++ Assets/Nakano/Scripts/ColumnBulletCreate.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnBulletCreate : MonoBehaviour
+{
+    [SerializeField] GameObject prefabs;
+    [SerializeField, Header("¶¬”")] int bulletNum;
+    [SerializeField, Header("ƒN[ƒ‹ƒ^ƒCƒ€")] float coolTime;
+    [SerializeField, Header("’e‘¬")] float speed;
+
+    [SerializeField, Header("Šp“x"), Tooltip("NormalBullets‚Åw’è‚µ‚½Šp“x‚æ‚è—Dæ‚³‚ê‚é")] float angle = 0;
+    [SerializeField, Header("c—ñ”"), Tooltip("‰¡‚É‰½ŒÂ•À‚Ô‚©(c—ñ‚ğ‰½ŒÂì‚é‚©) “ü—Í‚Í1ˆÈã")] int way = 1;
+    [SerializeField, Header("c—ñ“¯m‚Ì‹——£")] float distance;
+
+    NormalBullet normalBullet;
+
+    public bool isCreate = false;
+
+    void Start()
+    {
+        normalBullet = prefabs.GetComponent<NormalBullet>();
+        normalBullet.speed = speed;
+        if (way < 1) { way = 1; } //w’è‚³‚ê‚½way‚ª1–¢–‚Ì‚Æ‚«A1‚É‚·‚é
+
+    }
+
+    void Update()
+    {
+        if (isCreate)
+        {
+            isCreate = false;
+            StartCoroutine(Create());
+        }
+    }
+
+    IEnumerator Create()
+    {
+        for (int i = 0; i < bulletNum; i++)
+        {
+            if(way % 2 == 1)
+            {
+                for (int j = 0 - ((int)way / 2); j <= (way - ((int)way / 2) - 1); j++)
+                {
+                    GameObject obj = Instantiate(prefabs, this.transform.position + new Vector3(j * distance, 0, 0), Quaternion.identity);
+                    obj.GetComponent<NormalBullet>().angle = angle;
+                }
+            }
+            else if(way % 2 == 0)
+            {
+                for (int j = 0 - ((int)way / 2); j <= (way - ((int)way / 2)); j++)
+                {
+                    if(j < 0)
+                    {
+                        GameObject obj = Instantiate(prefabs, this.transform.position + new Vector3((j + 0.5f) * distance, 0, 0), Quaternion.identity);
+                        obj.GetComponent<NormalBullet>().angle = angle;
+                    }
+                    else if(j >= 0)
+                    {
+                        GameObject obj = Instantiate(prefabs, this.transform.position + new Vector3((j - 0.5f) * distance, 0, 0), Quaternion.identity);
+                        obj.GetComponent<NormalBullet>().angle = angle;
+                    }
+                }
+            }
+            
+            yield return new WaitForSeconds(coolTime);
+        }
+    }
+}
---
+++ Assets/Nakano/Scripts/PositionEdit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionEdit : MonoBehaviour
+{
+    public int num;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
