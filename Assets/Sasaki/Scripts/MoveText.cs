1a11cddf57fcd7a33e61fb0bfa7287bdf3eca3de
sdt22cge0009@edu.sca.ac.jp <sdt22cge0009@edu.sca.ac.jp>
2023-10-31 22:29:02 +0900

    タイムのUI設定完了

---
+++ Assets/Sasaki/Scripts/MoveText.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//テキストを順々に表示する
+public class MoveText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
