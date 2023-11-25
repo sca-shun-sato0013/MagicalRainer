f56bf521d07f46a1270a40b10681dbb7fccc6a62
goto <>
2023-11-25 23:27:47 +0900

    スキル　作成途中

---
+++ Assets/Goto/Scrips/Skill/SkillDefine.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDefine : MonoBehaviour
+{
+    [SerializeField]
+    private Skill skillSet1;
+    [SerializeField]
+    private Skill skillSet2;
+    [SerializeField]
+    private Skill skillSet3;
+    [SerializeField]
+    private Skill skillSet4;
+
+    public Skill SkiilStase1
+    {
+        set { skillSet1 = value; }
+    }
+    public Skill SkillStase2
+    {
+        set { skillSet2 = value; }
+    }
+    public Skill SkillStase3
+    {
+        set { skillSet3 = value; }
+    }
+    public Skill SkillStase4
+    {
+        set { skillSet4 = value; }
+    }
+    public enum Skill
+    {
+        _None,
+        Stargazer,
+        Pentagram,
+        Energyfunnel,
+        Mysticfield,
+        Skill5,
+        Skill6,
+    }
+
+    public static Dictionary<Skill, string> dic_SkillName = new Dictionary<Skill, string>()
+    {
+        {Skill._None, "スキル無し"},
+        {Skill. Stargazer, "スターゲイザー"},
+        {Skill.Pentagram, "ペンタグラム"},
+        {Skill.Energyfunnel, "エネルギーファンネル"},
+        {Skill.Mysticfield, "ミスティックフィールド"},
+        {Skill.Skill5, "スキル5"},
+        {Skill.Skill6, "スキル6"},
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        Skill skill= Skill._None;
+        Debug.Log(skill);
+        skillSet1 = Skill._None;
+    }
+
+    void SkillChange()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        SkillChange();
+    }
+ 
+}
