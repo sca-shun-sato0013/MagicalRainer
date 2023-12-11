4c9195be7e16a59eac94967b9138c8b831b927e1
goto <>
2023-12-11 20:15:05 +0900

    試し

--- Assets/Goto/Scrips/Skill/Drag/Skillname.cs
+++ Assets/Goto/Scrips/Skill/Drag/Skillname.cs
@@ -9,6 +9,15 @@ public class Skillname : MonoBehaviour
     [SerializeField]
     string skillset;
     string  skillname;
+
+    [SerializeField]
+    Skill skill1;
+    [SerializeField]
+    Skill skill2;
+    [SerializeField]
+    Skill skill3;
+    [SerializeField]
+    Skill skill4;
     // [SerializeField] private GameObject TargetSprite;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +30,7 @@ public class Skillname : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
 
         GameObject obj = collision.gameObject;
@@ -31,23 +40,51 @@ public class Skillname : MonoBehaviour
         SkillDefine skill = GameObject.Find("SkillManager").GetComponent<SkillDefine>();
         if(skillset=="1")
         {
-            skill.SkillSets[0] = (Skill)int.Parse(skillname);
             Debug.Log(skillname);
+            skill.SkillSets[0] = (Skill)int.Parse(skillname);
+            skill1= (Skill)int.Parse(skillname);
+        }
+        else
+        {
+            skill.SkillSets[0] = Skill._None;
+            skill1 = Skill._None;
         }
         if (skillset == "2")
         {
             skill.SkillSets[1] = (Skill)int.Parse(skillname);
-            Debug.Log(skillname);
+            skill2 = (Skill)int.Parse(skillname);
         }
-        if (skillset == "3")
+        else
+        {
+            skill.SkillSets[1] = Skill._None;
+            skill2 = Skill._None;
+        }
+
+
+        if (skillset == "3") 
+            
         {
             skill.SkillSets[2] = (Skill)int.Parse(skillname);
+            skill3 = (Skill)int.Parse(skillname);
         }
+        else
+        {
+            skill.SkillSets[2] = Skill._None;
+            skill3 = Skill._None;
+        }
+
         if (skillset == "4")
         {
             skill.SkillSets[3] = (Skill)int.Parse(skillname);
+            skill4 = (Skill)int.Parse(skillname);
+        }
+        else
+        {
+            skill.SkillSets[3] = Skill._None;
+            skill4 = Skill._None;
         }
 
 
     }
+
 }
